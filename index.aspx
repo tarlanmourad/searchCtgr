046882e397d75eef985bf574ade1b2d6b13348c7
Tarlan Muradli <terlanmuradli075@mail.ru>
2022-11-27 20:34:20 +0400

    Add project files.

---
+++ index.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="searchCtgr.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="CSS/style.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="frmContainer" runat="server">
+        <div>
+            <h1>Welcome!</h1>
+            <asp:Label ID="lblCtgr" runat="server" Text="Category Name"></asp:Label>
+            <asp:TextBox ID="txtCtgr" runat="server"></asp:TextBox>
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+            <asp:GridView ID="grdTable" CssClass="mydatagrid" PagerStyle-CssClass="pager" HeaderStyle-CssClass="header" RowStyle-CssClass="rows" runat="server"></asp:GridView>
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace searchCtgr
+{
+    public partial class index : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=TARLAN\\LOCALHOST;Initial Catalog=Northwind;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            string query = "SELECT * FROM prdctCtgr WHERE CategoryName = '" + txtCtgr.Text + "'";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            conn.Close();
+
+            if(ds.Tables[0].Rows.Count > 0)
+            {
+                grdTable.Visible = true;
+                lblResult.Text = "";
+                conn.Open();
+                string queryFill = "SELECT * FROM prdctCtgr WHERE CategoryName = '" + txtCtgr.Text + "'";
+                SqlCommand cmdx = new SqlCommand(queryFill, conn);
+                SqlDataAdapter sda = new SqlDataAdapter(cmdx);
+                DataTable dta = new DataTable();
+                sda.Fill(dta);
+                grdTable.DataSource = dta;
+                grdTable.DataBind();
+            }
+            else
+            {
+                grdTable.Visible = false;
+                lblResult.Text = "No matching row to this search";
+            }
+        }
+    }
+}
